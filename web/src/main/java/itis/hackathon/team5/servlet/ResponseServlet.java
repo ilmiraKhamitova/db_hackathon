c696818c452de63243a618f2ceea76610bff7e5b
ilmiraKhamitova <miragge.k@gmail.com>
2023-11-10 15:56:53 +0300

    servlet for getting signal data

---
+++ web/src/main/java/itis/hackathon/team5/servlet/ResponseServlet.java
@@ -0,0 +1,44 @@
+package itis.hackathon.team5.servlet;
+
+
+import com.google.gson.Gson;
+import itis.hackathon.team5.dao.SensorDao;
+import itis.hackathon.team5.dao.SignalDao;
+import itis.hackathon.team5.model.Sensor;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static itis.hackathon.team5.servlet.SignalServlet.*;
+import static itis.hackathon.team5.util.DatabaseConnectionUtil.getConnection;
+
+@WebServlet(name = "response", urlPatterns = "/respsignals")
+public class ResponseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Gson gson = new Gson();
+        Map<String, String> signals = new HashMap<>();
+        try {
+            List<Sensor> sensors = new SensorDao(getConnection(URL, USER, PASSWORD)).getAll();
+            for(Sensor s: sensors){
+                if (s.isWork()){
+                    signals.put(("Датчик №" + s.getId() + " (" + s.getId() + ")"), SignalDao.get(s.getId()));
+                } else {
+                    signals.put(("Датчик №" + s.getId() + " (" + s.getId() + ")"), "Датчик выключен");
+                }
+            }
+            resp.setContentType("application/json");
+            resp.getWriter().write(gson.toJson(signals));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- web/src/main/java/itis/hackathon/team5/servlet/SignalServlet.java
+++ web/src/main/java/itis/hackathon/team5/servlet/SignalServlet.java
@@ -1,14 +1,26 @@
 package itis.hackathon.team5.servlet;
 
+import itis.hackathon.team5.dao.SensorDao;
+import itis.hackathon.team5.model.Sensor;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
+import static itis.hackathon.team5.util.DatabaseConnectionUtil.getConnection;
+
+@WebServlet(name="signals", urlPatterns = "/signals")
 public class SignalServlet extends HttpServlet {
+    public static final String URL = "jdbc:postgresql://localhost:5432/hackathon";
+    public static final String USER = "test";
+    public static final String PASSWORD = "qwerty12345";
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.sendRedirect("/signal.ftl");
     }
 }
