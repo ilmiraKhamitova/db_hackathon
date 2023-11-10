5320da37d8b56900b710dd4483618d95fb1db26b
ngurevaa <nngurevaa@gmail.com>
2023-11-10 12:28:05 +0300

    refactor sensor table

--- scripts/V1.1__createSensorTable.sql
+++ scripts/V1.1__createSensorTable.sql
@@ -1,6 +1,6 @@
 create table sensor (
 	id bigint primary key,
 	type bigint references sensor_types (id) on delete cascade on update cascade,
-	work boolean,
-	date timestamp
+	work boolean default false not null,
+	date timestamp not null
 );
