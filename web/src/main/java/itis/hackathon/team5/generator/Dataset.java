3ed580956c589bdf5e87184433d92aea5409f8fd
liiceberg <2aysylu2003@gmail.com>
2023-11-10 11:56:55 +0300

    generator added

---
+++ web/src/main/java/itis/hackathon/team5/generator/Dataset.java
@@ -0,0 +1,23 @@
+package itis.hackathon.team5.generator;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dataset {
+    private final List<String> dataList;
+    public Dataset(String fileName) throws IOException {
+        dataList = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String s;
+            while ((s = reader.readLine()) != null) {
+                dataList.add(s);
+            }
+        }
+    }
+
+    public String getNext() {
+        return dataList.get((int) (Math.random() * dataList.size()));
+    }
+
+}
